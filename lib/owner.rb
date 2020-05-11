b0708701cb21271695610c9975082258c2e77056
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-11 23:53:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,6 +45,10 @@ class Owner
   end
 
   def walk_dogs
-    Dog.mood ="happy"
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
   end
 end
