1c928ab1b9495fb49fba10d5ba95288837cb69a3
Michael Adkins <mikeadkins1112@gmail.com>
2020-05-11 22:04:27 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,24 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+
+  def initialize(name, owner)
+    @name= name
+    @owner= owner
+    @mood= "nervous"
+    @@all << self
+    puts self
+   # binding.pry
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    count = self.all.length
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species, :count, :owner, :cats
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @cats = {:cat =>[]}
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    count= self.all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+
+    @cats = {:cat.name=>[]}
+  #  arra = Cat.all.select {|owner| owner.name == self}
+  #  @cats << arra
+  # Cat.all.each do |x|
+  #    if x.owner.name == self.name
+  #      puts self.name
+  #      puts x.name
+  #      @cats << x.name
+  #    end
+  #  end
+
+  end
 end
